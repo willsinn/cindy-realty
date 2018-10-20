59b567a92babff96133a13097c207128ce0683a3
willsinn <30275717+willsinn@users.noreply.github.com>
2018-10-20 00:53:23 -0400

    added hero layout according to wireframe

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,13 +9,30 @@ class Hero extends Component {
 
     return (
       <section className={classes.root}>
-        <p> This is Hero </p>
+        <div className={classes.header}>
+        </div>
+        <div className={classes.body}>
+          <div className={classes.menu}>
+          </div>
+          <div className={classes.info}>
+            <div className={classes.about}></div>
+            <div className={classes.mission}></div>
+            <div className={classes.certifications}></div>
+          </div>
+        </div>
       </section>
     );
   }
 }
 const styles = theme => ({
 root: {},
+header: {},
+body: {},
+menu: {},
+info: {},
+about: {},
+mission: {},
+certifications: {},
 });
 
 export default withStyles(styles, {})(Hero);
