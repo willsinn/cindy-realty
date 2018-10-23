899caece575db7c7373afc016867102ec025758a
willsinn <30275717+willsinn@users.noreply.github.com>
2018-10-22 22:52:01 -0400

    working on toggling pages with onClick handler

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,53 @@ import Main from './Components/Main';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isActivePage: false,
+      pages:
+    };
+    this.setState((isActivePage, props) => ({
+      return {
+        isActivePage:
+      }
+    })
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+  onToggleClick(e) {
+    e.preventDefault();
+    handleToggleClick = (e) => {
+      if (isActivePage === false) {
+        this.setState({
+        isActivePage: true
+      })} else {
+        return null;
+      }
+    }
+  }
+
   render() {
     const { classes } = this.props;
+
+    const renderActiveHero = (props) => {
+      return (
+        <Hero
+          isActivePage={isActivePage}
+        />
+      )
+    }
 
 
     return (
       <div className={classes.root}>
-          <Hero />
+          <Hero
+            isActivePage='true'
+            onToggleClick={this.handleToggleClick(e)}
+          />
+          <Main
+            isActivePage='false'
+            onClick={this.handleToggleClick(e)}
+          />
       </div>
     );
   }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,16 +3,26 @@ import { withStyles } from '@material-ui/core/styles';
 import HeroBg from '../Assets/HeroBg.jpeg';
 
 
+
 class Hero extends Component {
+  onToggleClick(e) {
+    this.props.handleToggleClick(e);
+  }
   render() {
     const { classes } = this.props;
 
-
     return (
       <section className={classes.root}>
-        <div className={classes.textContainer}>
-          <p className={classes.text}> Our city is defined by the people in it. What defines you </p>
+        <div className={classes.redirectContainer}>
+         <div className={classes.textContainer}>
+           <p className={classes.text}> Our city is defined by the people in it. What defines you </p>
+         <div className={classes.redirectButton}>
+           <button onClick={this.handleToggleClick.bind(e)}>
+             Living
+           </button>
+         </div>
         </div>
+      </div>
       </section>
     );
   }
