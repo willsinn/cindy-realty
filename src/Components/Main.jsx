a59917a97d6291c50480f6bc7cdb2301acde06e5
willsinn <30275717+willsinn@users.noreply.github.com>
2018-10-30 15:13:04 -0400

    created general layout for main page, renamed some class names and added filler text as placeholders

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import CindyProfile from '../Assets/CindyProfile.jpeg';
 import MainSloganBg from '../Assets/MainSloganBg.jpeg';
+import { Paper } from '@material-ui/core';
 
 
 class Main extends Component {
@@ -24,9 +25,24 @@ class Main extends Component {
           <div className={classes.menu}>
           </div>
           <div className={classes.info}>
-            <div className={classes.about}></div>
-            <div className={classes.mission}></div>
-            <div className={classes.certifications}></div>
+              <Paper className={classes.about}>
+                <h1 className={classes.aboutHeader}> about me </h1>
+                <p className={classes.aboutText}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quam justo, sodales vitae urna sit amet, volutpat suscipit tortor. Nulla ut consectetur augue. Duis dictum velit et metus volutpat condimentum ut vel mauris. Donec pretium lorem tempus pretium mollis. Sed sit amet pulvinar felis. Phasellus at egestas est. Cras a malesuada mauris, quis posuere ante. Praesent hendrerit felis non malesuada laoreet. Nunc justo ipsum, faucibus feugiat neque id, eleifend tincidunt ipsum. Morbi vitae condimentum risus. Curabitur lacinia orci sodales, imperdiet elit ut, feugiat libero. Curabitur posuere eros ipsum, at eleifend arcu consequat ut. Etiam accumsan ornare convallis.
+                </p>
+              </Paper>
+              <Paper className={classes.mission}>
+                <h1 className={classes.missionHeader}> my mission </h1>
+                <p className={classes.missionHText}>
+                Sed tempor erat ac tincidunt eleifend. Maecenas ornare augue dolor, vitae volutpat lorem tristique nec. Fusce accumsan vulputate egestas. Mauris euismod ex augue, sit amet facilisis urna tristique vitae. Vivamus erat sem, dictum sit amet faucibus et, convallis quis turpis. Suspendisse tristique tellus sollicitudin nisl consequat, nec vehicula dui viverra. Donec nec sagittis odio. Nullam quis nisi vitae sapien tristique mollis.
+                </p>
+              </Paper>
+              <Paper className={classes.certs}>
+                <h1 className={classes.certsHeader}> my certifications </h1>
+                <p className={classes.certsContent}>
+                Duis purus ligula, accumsan vitae risus non, vehicula dictum ante. In lobortis aliquam mollis. Suspendisse egestas nulla sed lorem facilisis, ut tincidunt ex luctus. Nam fringilla dui lacinia, condimentum nisl ut, iaculis libero. Integer molestie cursus metus non pellentesque. Suspendisse ac maximus odio. Aliquam rhoncus vel ante et placerat. Vestibulum consectetur volutpat dui non porta. Fusce ac velit non eros finibus cursus id eget neque. Sed ac vulputate tellus, et ornare purus.
+                </p>
+              </Paper>
           </div>
         </div>
       </section>
@@ -54,10 +70,25 @@ headerSloganWrapper: {
 slogan: {},
 body: {},
 menu: {},
-info: {},
+info: {
+  paddingRight: '15%',
+  paddingLeft: '15%',
+},
 about: {},
+aboutHeader: {
+  textTransform: 'uppercase',
+},
+aboutText: {},
 mission: {},
-certifications: {},
+missionHeader: {
+  textTransform: 'uppercase',
+},
+missionText: {},
+certs: {},
+certsHeader: {
+  textTransform: 'uppercase',
+},
+certsContent: {},
 });
 
 export default withStyles(styles, {})(Main);
